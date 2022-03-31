cfc85def1d9448d8c6a21582b142e0cb8d249b61
Earvin Lin <lin.earvin@gmail.com>
2022-03-31 14:56:18 +0800

    20220331-1531 test selenium.

--- testSeleniumByChrome.py
+++ testSeleniumByChrome.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
+# windows path
 #driver = webdriver.Chrome(executable_path=r'C:\Users\linea\AppData\Local\Programs\Python\Python38\chromedriver.exe')
+# linux path -- unidentified (2022.03.31) --
+#driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
+
 driver.get("https://goodinfo.tw/tw/index.asp")
 #assert("Python" in driver.title)
 
