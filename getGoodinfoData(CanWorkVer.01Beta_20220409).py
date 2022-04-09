32bb4ad44cf6ca02e0cda664de59bd49f4d7c72b
earvin(vivopc) <lin.earvin@gmail.com(vivopc)>
2022-04-09 22:11:22 +0800

    20220409-2210 test Goodinfo web data.

--- getGoodinfoData(CanWorkVer.01Beta_20220409).py
+++ getGoodinfoData(CanWorkVer.01Beta_20220409).py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# 設定broser profile(這支程式以firefox為範例，故只適用firefox browser)
+
 profile = webdriver.FirefoxProfile()
 #profile.set_preference("browser.download.folderList", 2)
 profile.set_preference("browser.download.manager.showWhenStarting", False)
@@ -12,14 +12,17 @@ profile.set_preference("browser.download.manager.showWhenStarting", False)
 #profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip")
 
 driver = webdriver.Firefox(firefox_profile=profile)
+#driver = webdriver.Firefox()
+
 driver.get("https://goodinfo.tw/tw/index.asp")
+#assert("Python" in driver.title)
 
 elem = driver.find_element_by_id("txtStockCode")
 #elem.clear()
 elem.send_keys("5388")
 elem.send_keys(Keys.RETURN)
 
-
+#assert("No results found." not in driver.page_source)
 """
 ### 下面寫的不能用…不知為何??
 try:
@@ -37,17 +40,16 @@ finally:
     web_element.click()
     print("Finished!!")
 """
-
-# 這種寫法，有時侯會因為網頁載入太慢(>10秒)而失敗
-driver.implicitly_wait(10)
+# Can work
+driver.implicitly_wait(10) # seconds
 web_element=driver.find_element_by_link_text('股利政策')
 web_element.click()
 
-# 這種寫法，有時侯會因為網頁載入太慢(>15秒)而失敗
-driver.implicitly_wait(15)
+driver.implicitly_wait(15) # seconds
+#elem2 = driver.find_element_by_xpath("//button[text()='匯出XLS']")
 button = driver.find_element_by_xpath("//input[@type='button' and @value='匯出XLS']")
 driver.execute_script("arguments[0].click();", button)
+#button.click()
 
-# 關閉browser
-driver.close() 
+#driver.close() # 關閉browser
 
