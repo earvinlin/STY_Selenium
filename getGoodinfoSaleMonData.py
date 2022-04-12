64fb9541f58876329a637d5fadc3f4a393705716
earvin(vivopc) <lin.earvin@gmail.com(vivopc)>
2022-04-12 21:12:35 +0800

    20220412-2112 Add get Goodinfo SaleMonth data.

--- getGoodinfoSaleMonData.py
+++ getGoodinfoSaleMonData.py
@@ -16,9 +16,9 @@ if len(sys.argv) < 2 :
 
 maxRetryCnt = 3
 stockCode = sys.argv[1]
-logFilename = "_errorlogDD.log"
+logFilename = "_errorlogSD.log"
 stockFilename = stockCode + ".xls"
-dividendFilename = "DividendDetail.xls"
+dividendFilename = "SaleMonDetail.xls"
 
 if not os.path.isfile(logFilename):
     open(logFilename, "a")
@@ -45,11 +45,13 @@ while (not isFinished):
     try:
         # 這種寫法，有時侯會因為網頁載入太慢(>10秒)而失敗
         driver.implicitly_wait(10)
-        web_element=driver.find_element_by_link_text('股利政策')
+#        web_element = driver.find_element_by_xpath("[@id='StockDetailMenu']/table/tbody/tr/td[1]/table/tbody/tr[7]/td/a")
+        web_element = driver.find_element_by_link_text('每月營收')
         web_element.click()
 
         # 這種寫法，有時侯會因為網頁載入太慢(>15秒)而失敗
         driver.implicitly_wait(15)
+#        button = driver.find_element_by_xpath("[@id='divSaleMonChartDetail']/table/tbody/tr/td/input[1]")
         button = driver.find_element_by_xpath("//input[@type='button' and @value='匯出XLS']")
         driver.execute_script("arguments[0].click();", button)
         
@@ -67,7 +69,6 @@ while (not isFinished):
         driver.close()
         if retryCnt > 3:
             isFinished = True
-
 
     #os.rename('/Users/earvin/Downloads/DividendDetail.xls', '/Users/earvin/Downloads/5388.xls')
     if os.path.isfile(dividendFilename):
