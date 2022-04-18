eb98fadce98764fb0166de389f574f08f60e7273
Earvinlin.02 <earvinlin.02@gmail.com>
2022-04-18 13:19:33 +0800

    20220418-1319 modified files.

--- _getStocksSaleMon_imac.sh
+++ _getStocksSaleMon_imac.sh
@@ -1,7 +1,19 @@
-# python3 getGoodinfoSaleMonData.py 0050
-# python3 getGoodinfoSaleMonData.py 0056
-python3 getGoodinfoSaleMonData.py 1101
-python3 getGoodinfoSaleMonData.py 1102
-python3 getGoodinfoSaleMonData.py 1210
-python3 getGoodinfoSaleMonData.py 1215
-python3 getGoodinfoSaleMonData.py 1232
+python3 getGoodinfoSaleMonData.py 6505
+python3 getGoodinfoSaleMonData.py 6523
+python3 getGoodinfoSaleMonData.py 6691
+python3 getGoodinfoSaleMonData.py 6706
+python3 getGoodinfoSaleMonData.py 6712
+python3 getGoodinfoSaleMonData.py 6803
+python3 getGoodinfoSaleMonData.py 8112
+python3 getGoodinfoSaleMonData.py 8341
+python3 getGoodinfoSaleMonData.py 8358
+python3 getGoodinfoSaleMonData.py 8416
+python3 getGoodinfoSaleMonData.py 8422
+python3 getGoodinfoSaleMonData.py 8436
+python3 getGoodinfoSaleMonData.py 8489
+python3 getGoodinfoSaleMonData.py 8942
+python3 getGoodinfoSaleMonData.py 9904
+python3 getGoodinfoSaleMonData.py 9910
+python3 getGoodinfoSaleMonData.py 9917
+python3 getGoodinfoSaleMonData.py 9925
+python3 getGoodinfoSaleMonData.py 9958
